8dabb2d22a595cda6142b6c5743c9f75bfa892d2
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-20 16:33:52 +0100

    migration definition

---
+++ database/migrations/2023_01_20_145528_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->text('thumb');
+            $table->unsignedMediumInteger('price');
+            $table->string('series', 150);
+            $table->date('sale_date');
+            $table->string('graphinc_novel', 150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
